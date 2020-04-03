ea7d1cddbae6efeec08d88b801fac418a96d12a8
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-04-03 09:08:01 -0700

    add zero rule to program.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,52 @@
+function roboSounds(number) {
+}
+
+
+
+
+
+
+
+
 $(document).ready(function(){
   $("form#formOne").submit(function(event) {
     event.preventDefault();
-    var number = paseInt($("input#userInput").val())
-  })
-});
+    
+    var number = parseInt($("input#userInput").val())
+    for(var index = 0; index <= number; index += 1) 
+      alert(index);
+      })
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// zero rule program will return a range of numbers from 0 to the users input number.
+// first rule numbers that contain a 1 all digits are replaced with the string "Beep!"
+// second rule numbers that contain a 2 all digits are replaced with the string "Boop!"
+// thrid rule numbers that contain a 3 all digits are replaced with the string "Won't you be my neighbor?"
+// fourth rule if a user inputs a number that contains both 1 & 2 the program will execute rule two, if a user inputs a number that contains 1,2 & 3 the program will execute rule 3. 
